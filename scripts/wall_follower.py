5584746b778011b076edbec5d30803f32ad9b911
matthias <matthiasling@uchicago.edu>
2022-04-12 21:55:07 +0000

    wall follower

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -9,14 +9,31 @@ from std_msgs.msg import Header
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Vector3
 
-from math import pi
+import math
+'''
+error = where we want - where we are
+kp = 0.1
+
+z = kp * error
+
+'''
+
 
 # x and z are linear and rotational velocity values
 # it makes it faster to type
-x = 0.2
+x = 0.1
 z = 0.2
 # A constant value for calculating what to modify velocity to 
-const = 0.1
+kp = 0.05
+
+def normalize_270(angle):
+    if angle in range(90,270): 
+        angle = angle - 270
+    elif angle in range(270,360):
+        angle = angle - 270
+    else:
+        angle = angle + 90
+    return angle
 
 #class for the first task - driving in a square based on a timer 
 class wall_follower(object):
@@ -33,8 +50,9 @@ class wall_follower(object):
         ang = Vector3()
         self.twist = Twist(linear=lin,angular=ang)
 
-        self.min_dist = 0.5
-    
+        self.min_dist = 0.75
+        # self.at_corner = False
+
     # The infinite function that is called
     def drive(self):
         rospy.spin()
@@ -43,49 +61,37 @@ class wall_follower(object):
 
         # md is the measured distance to the nearest wall
         # angle is the robot's angle to that nearest wall
-        md = min(data.ranges)
-        print(md)
+        md = float("inf")
+
+        for i in data.ranges:
+            if i > 0:
+                md = min(md,i)
+
         angle = data.ranges.index(md)
         
         # linear and angular are the speed components that I'll modify
         linear = self.twist.linear.x
         angular = self.twist.angular.z
 
-        # If the robot is too far for detection, just move forward
-        if md == float("inf"):
-            linear = x
-
         # If the robot is too close to the wall or at the threshold, move conditionally away from it 
         if md <= self.min_dist:
-            if angle in range(270, 360):
-                angular = z
-                linear = const * x * (360 - angle)/360
+            linear = x
+            angular = 0
 
-            if angle in range(180,270):
-                linear = x
-                angular = -z
-
-            if angle in range(90,180):
-                linear = 0
-                angular = -z
-            if angle in range(0,90):
-                linear = 0
-                angular = z
+            norm = normalize_270(angle)
+           
+            angular = kp*(norm)/2.5
 
         # If the robot is too far from the wall, move conditionally towards it 
-        if md > self.min_dist:
-            if angle in range(0,90):
+        else:        
+            
+            if angle > 180: 
+                angle -= 360
+            if angle in range(-90,90):
                 linear = x
-                angular = z
-            if angle in range(90,180):
-                linear = const * x *(180-angle)/180
-                angular = z
-            if angle in range(180,270):
-                angular = -z
-                linear = const * x * (angle-180)/180
-            if angle in range(270,360):
-                linear = x
-                angular = z
+
+            angular = kp*(angle)/2.5
+            
         self.twist.linear.x = linear
         self.twist.angular.z = angular
         self.publisher.publish(self.twist)
